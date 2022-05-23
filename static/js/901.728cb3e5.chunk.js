ea773fcb1ecd89632739c39babb651d4bd379197
Zolotarova-Nataliia <zolotarova.nataliia@gmail.com>
2022-05-23 15:54:14 +0300

    Updates

---
+++ static/js/901.728cb3e5.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[901],{4901:function(e,o,n){n.r(o),n.d(o,{default:function(){return h}});var t=n(9434),c=n(4289),r=n(7371),i=n(184);function h(){var e=(0,t.v9)(c.vW);return(0,i.jsx)("div",{children:(0,i.jsxs)(r.Dx,{children:["Welcome to Phonebook aplication ",e,"!"]})})}}}]);
+//# sourceMappingURL=901.728cb3e5.chunk.js.map
